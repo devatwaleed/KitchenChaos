c37e697e109db83b7309c8f74a9ffe4093cad271
devatwaleed <129673627+devatwaleed@users.noreply.github.com>
2023-05-16 18:17:25 +0500

    3:33:00

--- KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour{
-    
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
+
     public void interact(){
-        Debug.Log("Interact");
+        if (kitchenObject == null){
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.localPosition = Vector3.zero;
+            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObject.SetClearCounter(this);
+        }else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
     }
 }

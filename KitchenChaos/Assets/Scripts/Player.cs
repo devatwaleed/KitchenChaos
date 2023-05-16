3662ffba4b42c9c30b606e8614d1aa266aa7c0c1
devatwaleed <129673627+devatwaleed@users.noreply.github.com>
2023-05-16 16:37:20 +0500

    SelectedCounterVisual
    3:11:00;

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private static Player instance;
+    public static Player Instance { get; private set; }
+
+    public event EventHandler <OnSelectedCounterChangeEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangeEventArgs : EventArgs {
+        public ClearCounter selectedCounter;
+    }
+
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask countersLayerMask;
@@ -11,6 +20,13 @@ public class Player : MonoBehaviour
     private bool isWalking;
     private Vector3 lastInteractDir;
     private ClearCounter selectedCounter;
+
+    private void Awake(){
+        if (Instance != null) {
+            Debug.LogError("There is more than one player instance");
+        }
+        Instance = this;
+    }
 
     private void Start(){
         gameInput.OnInteractAction += GameInput_OnInteractAction;
@@ -46,12 +62,13 @@ public class Player : MonoBehaviour
                 //clearCounter.interact();
                 if (clearCounter != selectedCounter) {
                     selectedCounter = clearCounter;
+                    SetSelectedCounter(clearCounter);
                 }
             }else{
-                selectedCounter = null;
+                SetSelectedCounter(null);
             }
         }else{
-            selectedCounter = null;
+            SetSelectedCounter(null);
         }
         Debug.Log(selectedCounter);
     }
@@ -104,6 +121,14 @@ public class Player : MonoBehaviour
         // transform the rotation of the player according to the direction in which the player is using slerp (used for rotation) and mul by rotateSpeed for extra speed.
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter){
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangeEventArgs{
+            selectedCounter = selectedCounter
+        });
     }
 
 }
